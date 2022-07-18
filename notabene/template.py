e3be04a08fabf801540531db600951c4a461a8d5
Jeroen Peter Bos <jeroen@notabene.cloud>
2022-07-18 19:25:34 +0200

    Implemented most of the basic create template functionality

--- notabene/template.py
+++ notabene/template.py
@@ -8,21 +8,9 @@ from typing import List
 import click
 
 from notabene.base import Project, base
+from notabene.utils import is_snake_case
 
 log = logging.getLogger(__name__)
-
-
-@base.group()
-@click.option(
-    "--template-dir",
-    "-t",
-    type=click.Path(exists=True, file_okay=False),
-)
-@click.pass_obj
-def template(project: Project, template_dir: click.Path):
-    """Use, check and create templates."""
-    if template_dir is not None:
-        project.template_dir = template_dir
 
 
 def _list_templates(template_dir: Path):
@@ -35,33 +23,7 @@ def _echo_templates(templates: List[Path]):
         click.echo(f"[{i:>2}]\t{path.stem}")
 
 
-@template.command(name="list")
-@click.pass_obj
-def list_command(project: Project):
-    """List all of your templates."""
-    _echo_templates(_list_templates(project.template_dir))
-
-
-@template.command()
-@click.option(
-    "--template",
-    "-t",
-    type=str,
-    default="",
-    help="The name or index of a template. An option prompt is shown by default.",
-)
-@click.argument("notebook", type=str)
-@click.pass_obj
-def use(project: Project, template: str, notebook: str):
-    """Create a new notebook using one of your templates.
-
-    NOTEBOOK is the name of the new notebook. Adding `.ipynb` is optional.
-    """
-    notebook = Path(notebook).with_suffix(".ipynb")
-    if notebook.exists():
-        raise click.BadArgumentUsage(f"The file '{notebook}' already exists.")
-
-    templates = _list_templates(project.template_dir)
+def _select_template(templates: List[Path], template: str):
     if template == "":
         _echo_templates(templates)
         while True:
@@ -90,12 +52,109 @@ def use(project: Project, template: str, notebook: str):
                 message=f"Template name '{template}' does not exist. "
                 "Leave empty or call the list command to get a list of options.",
             )
-    log.info("Using template located at %s", template_path)
+    log.info("Selected template located at %s", template_path)
+    return template_path
+
+
+@base.group()
+@click.option(
+    "--template-dir",
+    "-t",
+    type=click.Path(exists=True, file_okay=False),
+)
+@click.pass_obj
+def template(project: Project, template_dir: click.Path):
+    """Use, check and create templates."""
+    if template_dir is not None:
+        project.template_dir = template_dir
+
+
+@template.command()
+@click.option(
+    "--name",
+    "-n",
+    type=str,
+    default="",
+    help="The name of the new template. An option prompt is shown if left empty.",
+)
+@click.argument("notebook", type=str)
+@click.pass_obj
+def create(project: Project, name: str, notebook: str):
+    """Create a new template."""
+    templates = _list_templates(project.template_dir)
+
+    if name == "":
+        while True:
+            name = click.prompt("Pick a name for the template", type=str)
+            if is_snake_case(name):
+                if name not in [t.stem for t in templates]:
+                    break
+                else:
+                    click.echo(f"Error: The template '{name}' already exists.")
+            else:
+                click.echo(
+                    "Error: The name of the template should be snake case. "
+                    "For example: 'the_name_of_the_template'"
+                )
+
+    elif name in [t.stem for t in templates]:
+        raise click.BadOptionUsage(
+            option_name="name", message=f"A template with name '{name}' already exists."
+        )
+    template_path = (project.template_dir / name).with_suffix(".ipynb")
+
+    if notebook == "":
+        # Select one of the notabene templates
+        # https://stackoverflow.com/questions/6028000/how-to-read-a-static-file-from-inside-a-python-package
+        # Implement this later
+        pass
+    elif not Path(notebook).with_suffix(".ipynb").exists():
+        raise click.BadArgumentUsage(
+            f"The notebook '{Path(notebook).with_suffix('.ipynb')}' does not exist."
+        )
+    else:
+        notebook = Path(notebook).with_suffix(".ipynb")
+
+    shutil.copy(notebook, template_path)
+    click.secho(
+        f"Created template '{name}' using notebook '{notebook}'.", fg="cyan", bold=True
+    )
+
+
+@template.command(name="list")
+@click.pass_obj
+def list_command(project: Project):
+    """List all of your templates."""
+    _echo_templates(_list_templates(project.template_dir))
+
+
+@template.command()
+@click.option(
+    "--template",
+    "-t",
+    type=str,
+    default="",
+    help="The name or index of a template. An option prompt is shown by default.",
+)
+@click.argument("notebook", type=str)
+@click.pass_obj
+def use(project: Project, template: str, notebook: str):
+    """Use one of your templates to create a new notebook.
+
+    NOTEBOOK is the name of the new notebook. Adding `.ipynb` is optional.
+    """
+    notebook = Path(notebook).with_suffix(".ipynb")
+    if notebook.exists():
+        raise click.BadArgumentUsage(f"The file '{notebook}' already exists.")
+
+    templates = _list_templates(project.template_dir)
+    template_path = _select_template(templates=templates, template=template)
 
     notebook.parent.mkdir(parents=True, exist_ok=True)
     shutil.copy(template_path, notebook)
+
     click.secho(
-        f"Created new notebook '{notebook}' using template '{template_path.stem}'.",
+        f"Created notebook '{notebook}' using template '{template_path.stem}'.",
         fg="cyan",
         bold=True,
     )
